11fb37a5b1fd6fe20696e8e8f46e0a628983b51c
sabroh28 <88585944+sabroh28@users.noreply.github.com>
2022-01-26 15:18:32 +0530

    Add files via upload

---
+++ Speech Recognizer.py
@@ -0,0 +1,16 @@
+from ast import Try
+from multiprocessing.connection import Listener
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+
+def takecommand():
+    try:
+        with sr.Microphone as source:
+            voice = listener.listen(source)
+            query = listener.recognize_google(voice)
+            query = query.lower
+
+    except:
+        pass
+    return query
